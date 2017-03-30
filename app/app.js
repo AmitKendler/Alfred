7d9f5a04c13e9d20e454e259079fe52e29318973
Matan <20matan@gmail.com>
2017-03-30 11:21:17 +0300

    Created app file and some folders

---
+++ app/app.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+const app = () => (
+  <View>
+    <Text>
+      Hello world
+    </Text>
+  </View>
+)
+
+export default app;
