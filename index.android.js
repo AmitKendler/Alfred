9ab70d722865688822ccb774364a2a59334ff8da
Matan <20matan@gmail.com>
2017-03-30 21:41:42 +0300

    aa

--- index.android.js
+++ index.android.js
@@ -2,5 +2,5 @@ import { AppRegistry } from 'react-native';
 import alfredFront from './app/app';
 import matanApp from './app/matantest';
 
-AppRegistry.registerComponent('alfredFront', () => alfredFront);
-//AppRegistry.registerComponent('alfredFront', () => matanApp);
+// AppRegistry.registerComponent('alfredFront', () => alfredFront);
+AppRegistry.registerComponent('alfredFront', () => matanApp);
